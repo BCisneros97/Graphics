c144e0940288b54d8317de58d2263b81fc941e81
Benny Cisneros <benycisneros@gmail.com>
2018-09-30 21:27:35 -0500

    figura16

--- figuras.c
+++ figuras.c
@@ -47,6 +47,26 @@ void figura15(int xc, int yc, int size) {
     line(x1 + 6*size, y1 + 3*size, x1 + 8*size, yc);
 }
 
+void figura16(int xc, int yc, int size) {
+    int x1 = xc - size, y1 = yc - size;
+
+    line(x1, y1, x1, y1 - 3*size);
+    y1 -= 3*size;
+    line(x1, y1, x1 - size, y1);
+    x1 -= size;
+    line(x1, y1, xc, y1 - 2*size);
+    y1 -= 2*size;
+    x1 = xc;
+    line(x1, y1, x1 + 2*size, y1 + 2*size);
+    x1 += 2*size;
+    y1 += 2*size;
+    line(x1, y1, x1 - size, y1);
+    x1 -= size;
+    line(x1, y1, x1, y1 + 5*size);
+    y1 += 5*size;
+    line(x1, y1, x1 - 5*size, y1);
+}
+
 int main(int argc, char *argv[]) {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
@@ -54,6 +74,7 @@ int main(int argc, char *argv[]) {
     figura13(20, 20, 20);
     figura14(80, 120, 10);
     figura15(80, 170, 10);
+    figura16(80, 270, 10);
 
     getch();
     closegraph();
