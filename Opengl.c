939608dad3abbb44d47a6436752ce9ee35249924
Ruben <g.carlo97@hotmail.com>
2018-10-08 16:08:32 -0500

    Otro ejemplo :V

--- Opengl.c
+++ Opengl.c
@@ -1,98 +1,143 @@
+/******* Inicio de las bibliotecas *******/
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
 
-GLboolean doubleBuffer;
+/******* Fin de las bibliotecas *******/
 
-/* ARGSUSED1 */
-static void Key(unsigned char key, int x, int y){
-  switch (key) {
-  case 27:
-    exit(0);
-  }
+/******Prototipos de función void display(); ***/
+
+void specialKeys();
+// Variables globales
+double rotate_y=0;
+double rotate_x=0;
+
+/******* Fin de la función display *******/
+
+/*****Función de retrollamada “display()” *****/
+
+void display()
+{
+// Borrar pantalla y Z-buffer
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+// Resetear transformaciones
+glLoadIdentity();
+
+/******* Fin de la funcion  retrollamdo *******/
+
+/*****  Rotar cuando el usuario cambie “rotate_x” y “rotate_y” *****/
+
+glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+
+/******* Fin de la función rotar  *******/
+
+/*Inicio de la ubicación y pigmentación de las ras o tapas del cubo*/
+//LADO FRONTAL: lado multicolor
+
+glBegin(GL_POLYGON);
+
+glColor3f( 1.0, 0.0, 0.0 ); glVertex3f( 0.5, -0.5, -0.5 ); // P1 es rojo
+glColor3f( 0.0, 1.0, 0.0 ); glVertex3f( 0.5, 0.5, -0.5 ); // P2 es verde
+glColor3f( 0.0, 0.0, 1.0 ); glVertex3f( -0.5, 0.5, -0.5 ); // P3 es azul
+glColor3f( 1.0, 0.0, 1.0 ); glVertex3f( -0.5, -0.5, -0.5 ); // P4 es morado
+
+glEnd();
+
+
+/*FIN DE LA CARA 1*/
+// LADO TRASERO: 
+glBegin(GL_POLYGON);
+
+glColor3f( -1.0, 1.0, -1.6 ); // color de la cara o tapa
+glVertex3f( 0.5, -0.5, 0.5 );
+glVertex3f( 0.5, 0.5, 0.5 );
+glVertex3f( -0.5, 0.5, 0.5 );
+glVertex3f( -0.5, -0.5, 0.5 );
+
+glEnd();
+
+// LADO DERECHO:
+glBegin(GL_POLYGON);
+glColor3f( 1.0, 0.5, 0.1 ); // color de la cara o tapa
+glVertex3f( 0.5, -0.5, -0.5 );
+glVertex3f( 0.5, 0.5, -0.5 );
+glVertex3f( 0.5, 0.5, 0.5 );
+glVertex3f( 0.5, -0.5, 0.5 );
+glEnd();
+
+// LADO IZQUIERDO: 
+glBegin(GL_POLYGON);
+glColor3f( 1.0, -2.8, 1.0 ); // color de la cara o tapa
+glVertex3f( -0.5, -0.5, 0.5 );
+glVertex3f( -0.5, 0.5, 0.5 );
+glVertex3f( -0.5, 0.5, -0.5 );
+glVertex3f( -0.5, -0.5, -0.5 );
+glEnd();
+// LADO SUPERIOR: 
+glBegin(GL_POLYGON);
+glColor3f( 1.0, -4.0, -1.0 ); // color de la cara o tapa
+glVertex3f( 0.5, 0.5, 0.5 );
+glVertex3f( 0.5, 0.5, -0.5 );
+glVertex3f( -0.5, 0.5, -0.5 );
+glVertex3f( -0.5, 0.5, 0.5 );
+glEnd();
+// LADO INFERIOR: 
+glBegin(GL_POLYGON);
+glColor3f( 1.0, -3.8, -1.0); // color de la cara o tapa
+glVertex3f( 0.5, -0.5, -0.5 );
+glVertex3f( 0.5, -0.5, 0.5 );
+glVertex3f( -0.5, -0.5, 0.5 );
+glVertex3f( -0.5, -0.5, -0.5 );
+glEnd();
+
+glFlush(); //NOTA: dan el efecto de doble-buffer.
+glutSwapBuffers(); //NOTA: dan el efecto de doble-buffer.
 }
 
-static void Draw(void) {
-  glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+/*fin de la ubicación y pigmentación de las ras o tapas del cubo*/
 
-  glStencilFunc(GL_ALWAYS, 1, 1);
-  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+// Función de retrollamada “specialKeys()”
+void specialKeys( int key, int x, int y )
+{
+// La flecha derecha: incrementa su rotación en 5 grados
+if (key == GLUT_KEY_RIGHT)
+rotate_y += 5;
+// La flecha izquierda: disminuye su rotación en 5 grados
+else if (key == GLUT_KEY_LEFT)
+rotate_y -= 5;
 
-  /* Triangulo Rojo */
-  glColor3ub(200, 0, 0);
-  glBegin(GL_POLYGON);
-  glVertex3i(-4, -4, 0);
-  glVertex3i(4, -4, 0);
-  glVertex3i(0, 4, 0);
-  glEnd();
+else if (key == GLUT_KEY_UP)
+rotate_x += 5;
 
-  glStencilFunc(GL_EQUAL, 1, 1);
-  glStencilOp(GL_INCR, GL_KEEP, GL_DECR);
-
-  /* Cuadrado Amarillo */
-  glColor3ub(0, 200, 0);
-  glBegin(GL_POLYGON);
-  glVertex3i(3, 3, 0);
-  glVertex3i(-3, 3, 0);
-  glVertex3i(-3, -3, 0);
-  glVertex3i(3, -3, 0);
-  glEnd();
-
-  glStencilFunc(GL_EQUAL, 1, 1);
-  glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-
-  /* Cuadrado Azul */
-  glColor3ub(255, 255, 0);
-  glBegin(GL_POLYGON);
-  glVertex3i(3, 3, 0);
-  glVertex3i(-3, 3, 0);
-  glVertex3i(-3, -3, 0);
-  glVertex3i(3, -3, 0);
-  glEnd();
-
-  if (doubleBuffer) {
-    glutSwapBuffers();
-  } else {
-    glFlush();
-  }
+else if (key == GLUT_KEY_DOWN)
+rotate_x -= 5;
+// Solicitud para actualizar la pantalla
+glutPostRedisplay();
 }
 
-static void Args(int argc, char **argv) {
-  GLint i;
-  doubleBuffer = GL_TRUE;
-  for (i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-sb") == 0) {
-      doubleBuffer = GL_FALSE;
-    } else if (strcmp(argv[i], "-db") == 0) {
-      doubleBuffer = GL_TRUE;
-    }
-  }
-}
-
-int main(int argc, char **argv) {
-  GLenum type;
-
-  glutInit(&argc, argv);
-  Args(argc, argv);
-
-  type = GLUT_RGB | GLUT_STENCIL;
-  type |= (doubleBuffer) ? GLUT_DOUBLE : GLUT_SINGLE;
-  glutInitDisplayMode(type);
-  glutCreateWindow("Stencil Test");
-
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glClearStencil(0);
-  glStencilMask(1);
-  glEnable(GL_STENCIL_TEST);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-5.0, 5.0, -5.0, 5.0, -5.0, 5.0);
-  glMatrixMode(GL_MODELVIEW);
-
-  glutKeyboardFunc(Key);
-  glutDisplayFunc(Draw);
-  glutMainLoop();
-  return 0;             
+// Función main()
+int main(int argc, char* argv[])
+{
+// Inicializar los parámetros GLUT y de usuario proceso
+glutInit(&argc,argv);
+// Solicitar ventana con color real y doble buffer con Z-buffer
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+// Crear ventana
+glutCreateWindow(“Prueba”); // titulo del cuadro de ejecución 
+// Habilitar la prueba de profundidad de Z-buffer
+glEnable(GL_DEPTH_TEST);
+// Funciones de retrollamada
+glutDisplayFunc(display); glutSpecialFunc(specialKeys);
+// Pasar el control de eventos a GLUT
+glutMainLoop();
+// Regresar al sistema operativo
+return 0;
 }
